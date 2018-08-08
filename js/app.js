b91b0dde6866cd3532bb306b424b07224914895b
SarahWinchester <indira_ruli@hotmail.com>
2018-08-08 10:56:58 -0500

    commit

--- js/app.js
+++ js/app.js
@@ -1,8 +1,35 @@
 //search Function
-$(document).ready(function(){
+
     $("#search-btn").click(function(){
         let inputSearchVal = $("#search-input").val();
         console.log(inputSearchVal);
-        searchingRequest(inputSearchVal);
+        // mercado(inputSearchVal);
+        // searchingRequest(inputSearchVal);
     })
-})
+// })
+// const mercado = () => {
+//     $.ajax({
+//         url: `https://api.mercadolibre.com/sites/MLA/search?q=goth`,
+//         type: 'GET',
+//         crossDomain: true,
+//         datatype: 'json',
+//         success: function (response) {
+//             for (var i = 0; i <= 5; i++) {
+//                 var photo = response.results[i].thumbnail;
+//                 console.log(photo);
+//             }
+//         }
+//     })
+// }
+                //Function for the API for the seach 
+                // function searchingRequest(inputSearchVal) { 
+                //     let getsearchingUrl = "https://api.mercadolibre.com/sites/MLM/search?q="+ inputSearchVal;
+                //     $.ajax({
+                //         url: getsearchingUrl,
+                //         type: "GET",
+                //         crossDomain: true,
+                //     }).done(function(response) {
+                //         console.log(response);
+                //     })
+                // }; 
+                
