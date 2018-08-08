6625f24659f5661e3d07b0de3365eb59e92b08d9
SarahWinchester <indira_ruli@hotmail.com>
2018-08-08 13:08:09 -0500

    the search function replace spaces with underscores, and mainshow added

--- js/app.js
+++ js/app.js
@@ -24,7 +24,9 @@ $(".category").click(function(event){
 
 //Function for the API for the seach 
 function searchingRequest(inputSearchVal) { 
-    let getsearchingUrl = "https://api.mercadolibre.com/sites/MLM/search?q="+ inputSearchVal;
+    let searchWithscores = inputSearchVal.replace(" ","_");
+    console.log(searchWithscores);
+    let getsearchingUrl = "https://api.mercadolibre.com/sites/MLM/search?q="+ searchWithscores;
     $.ajax({
         url: getsearchingUrl,
         type: "GET",
@@ -33,7 +35,6 @@ function searchingRequest(inputSearchVal) {
         console.log(response);
     })
 }; 
-
 //Function for the category
 function categoryRequest(categorySelected) { 
     let getsearchingUrlCategory = "https://api.mercadolibre.com/sites/MLM/search?q="+ categorySelected;
@@ -43,6 +44,18 @@ function categoryRequest(categorySelected) {
         crossDomain: true,
     }).done(function(responseCategory) {
         console.log(responseCategory);
+        //Aqui debo esconder la data anterior que esta por default en la pagina en cada uno de los else.
+    })
+}; 
+
+function mainShow() { 
+    let getMainShow = "https://api.mercadolibre.com/sites/MLM/search?q=moda-alternativa";
+    $.ajax({
+        url: getMainShow,
+        type: "GET",
+        crossDomain: true,
+    }).done(function(responseMain) {
+        console.log(responseMain);
         //Aqui debo esconder la data anterior que esta por default en la pagina en cada uno de los else.
     })
 }; 
