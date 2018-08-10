5cb7f84c95eca957c19a5cff4f497164da78ead3
SarahWinchester <indira_ruli@hotmail.com>
2018-08-10 10:34:27 -0500

    trying to separate modals

--- js/app.js
+++ js/app.js
@@ -253,98 +253,56 @@ function searchingRequest(inputSearchVal) {
         url: getsearchingUrl,
         type: "GET",
         crossDomain: true,
-    }).done(function(response) {
-        console.log(response);
+    }).done(function(responseSearch) {
+        console.log(responseSearch);
         $("#itemsContainer").html("");
+        searchingRequestFill(responseSearch);
+        
     })
-    // searchingRequest(response);
     
 }; 
 
-// function searchingRequest(response){
-//     for(let i = 0 ; i < (response.results).length; i+=1){
-//         console.log(i);
-//         console.log(response.results);
+function searchingRequestFill(responseSearch){
+    console.log(responseSearch.results);
+    
+    for(let i = 0 ; i < (responseSearch.results).length; i+=1){
+        console.log(i);
+        console.log(responseSearch.results);
 
-
-//     }
-// }
-
-
-// let principalTemplateSearch=` <!--Cards--> <div class="card margincard cardstyle d-inline-block" style="width: 15rem;">
-// <img class="card-img-top" src="{{thumbnail}}" alt="Card image cap">
-// <div class="card-body">
-// <h5 class="card-title">{{title}}</h5>     
-// <h4>{{price}}<h4>               
-// <!--Launch Demo Modal-->
-// <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
-// Detalles
-// </button>        
-// </div>
-// </div>
-// <!--Ends Card-->`
-
-// let SearchModalBox = `<!-- Modal -->
-// <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-// <div class="modal-dialog" role="document">
-// <div class="modal-content">
-// <div class="modal-header">
-// <h5 class="modal-title" id="exampleModalLongTitle ">{{titleModal}}</h5>
-// <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-// <span aria-hidden="true">&times;</span>
-// </button>
-// </div>
-// </div>
-// <div class="modal-body">
-// <div class="row">
-// <img class=" col-4 offset-4 img-fluid"  src="{{thumbnailModal}}" alt="">
-// </div>
-// <div class="row itemsTitles">
-// <span class="col-3">ID</span>
-// <span class="col-3">Lugar de Origen</span>
-// <span class="col-3">Existencias</span>
-// <span class="col-3">Precio</span>
-// </div>
-// <div class="row">
-// <span class="col-3">{{idModal}}</span>
-// <span class="col-3">{{whereModal}}</span>
-// <span class="col-3">{{stockModal}}</span>
-// <span class="col-3">{{priceModal}}</span>
-// </div>
-// <div class="row marginBtnsModal">
-// <button class="btn btn-outline-success my-2 my-sm-0 searchbtn col-4 offset-1" id="search-btnModalAdd" type="button">Zhoping Cart</button>
-// <button class="btn btn-outline-success my-2 my-sm-0 searchbtn col-4 offset-1" id="search-btnBuy" type="button">Buy</button>
-
-// </div>
-// </div>
-// <div class="modal-footer">
-// <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-// </div>
-// </div>`
-
-
-//used for a good control on replace
-
-// let titleSearch = response.results[i].title;
-// let thumbnailSearch= response.results[i].thumbnail;
-// let idSearch = response.results[i].id;
-// let priceSearch= response.results[i].price;
-// let placeSearch= response.results[i].address.state_name;
-// let stockSearch= response.results[i].available_quantity;
-
-
-// let fillingTemplateSearch= principalTemplateSearch.replace("{{thumbnail}}", thumbnailSearch)
-// .replace("{{title}}",titleSearch).replace("{{price}}", priceSearch);
-
-// let fillingTemplateSearchModal= SearchModalBox.replace("{{thumbnailModal}}", thumbnailSearch)
-// .replace("{{titleModal}}", titleSearch)
-// .replace("{{idModal}}", idSearch )
-// .replace("{{whereModal}}",placeSearch)
-// .replace("{{stockModal}}",stockSearch)
-// .replace("{{priceModal}}", priceSearch);
-
-// $("#itemsContainer").append(fillingTemplateSearch);
-// $("#itemsContainer").append(fillingTemplateSearchModal);
-//     }                   
-
-// };        
+        let principalTemplateSearch=` <!--Cards--> <div class="card margincard cardstyle d-inline-block" style="width: 15rem;">
+        <img class="card-img-top" src="{{thumbnail}}" alt="Card image cap">
+        <div class="card-body">
+        <h5 class="card-title">{{title}}</h5>     
+        <h4>{{price}}<h4>               
+        <!--Launch Demo Modal-->
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+        Detalles
+        </button>        
+        </div>
+        </div>
+        <!--Ends Card-->`
+        
+        //used for a good control on replace
+        
+        let titleSearch = responseSearch.results[i].title;
+        let thumbnailSearch= responseSearch.results[i].thumbnail;
+        let idSearch = responseSearch.results[i].id;
+        let priceSearch= responseSearch.results[i].price;
+        let placeSearch= responseSearch.results[i].address.state_name;
+        let stockSearch= responseSearch.results[i].available_quantity;
+        
+        
+        let fillingTemplateSearch= principalTemplateSearch.replace("{{thumbnail}}", thumbnailSearch)
+        .replace("{{title}}",titleSearch).replace("{{price}}", priceSearch);
+        
+        $("#idModal").html(idSearch);
+        $("#whereModal").html(placeSearch);
+        $("#stockModal").html(stockSearch);
+        $("#priceModal").html(priceSearch);
+        $("#exampleModalLongTitle").html(titleSearch);
+        
+        
+        $("#itemsContainer").append(fillingTemplateSearch);
+    }                   
+    
+};        
